e657df662b3e282e606e24668f6aa3641470b667
Charicauth <50031787+charicauth@users.noreply.github.com>
2023-12-24 00:49:12 +0100

    Correction d'affichage + Fermeture de la modale + gestion d'affichage de l'erreur du formulaire

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,64 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelectorAll(".close");
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+closeBtn.forEach((span) => span.addEventListener("click", closeModal));
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
+// Close modal
+function closeModal () {
+  modalbg.style.display = "none";
+}
+
+// Form management
+//  Form elements
+var nameError = document.getElementById("prenom-error");
+var lastNameError = document.getElementById("nom-error");
+var emailError = document.getElementById("email-error");
+var birthdateError = document.getElementById("birthdate-error");
+var nbTournoisError = document.getElementById("nbtournois-error");
+var selectError = document.getElementById("select-error");
+var conditionsError = document.getElementById("conditions-error");
+
+// Form validation function
+function validate() {
+
+}
+
+// check email
+function checkName() {
+  var name = document.getElementById("first").value;
+
+  if (name.length == 0) {
+    nameError.innerHTML = "veuillez renseigner votre prénom";
+    return false;
+  }
+  if (name.length < 2 || name.length > 20) {
+    nameError.innerHTML = "votre prénom doit compris entre 2 et 20 caracteres";
+    return false;
+  }
+  if (name.match(/\d/)) {
+    nameError.innerHTML = "Votre prénom ne doit pas contenir de chiffres";
+    return false;
+  }
+
+  nameError.innerHTML = "";
+  return true;
+}
+function checkLastName() {
+
+}
+function checkEmail() {}
+function checkBirthdate() {}
+function checkNbTournois() {}
+function checkSelect() {}
+function checkConditions() {}
 
